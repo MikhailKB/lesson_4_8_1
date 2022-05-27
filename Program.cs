4076b67e1e5cae184a37690c91a4143234180d51
Михаил Козин <Vasago85@mail.ru>
2022-05-28 00:20:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace lesson_4_8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  // точка входа
+        {
+
+            Write("Введите количество строк и нажмите \"Enter\": ");
+            int lines = int.Parse(ReadLine());
+            Write("Введите количество столбцов и нажмите \"Enter\": ");
+            int colums = int.Parse(ReadLine());
+
+            var A = new int[lines, colums];
+
+            Random r = new Random();
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    A[i, j] = r.Next(1, 5);
+                    sum += A[i, j];
+                    Write($"{A[i, j],2}");
+                }
+                WriteLine($" : {sum}  {"Сумма"}");
+            }
+            ReadKey();
+        }
+    }
+}
